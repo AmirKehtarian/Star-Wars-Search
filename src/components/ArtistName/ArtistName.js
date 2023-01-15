bebadf985ba8bba7fcbb9da9c825b0964e827bf5
AmirKehtarian <110422774+AmirKehtarian@users.noreply.github.com>
2023-01-15 09:35:45 +0100

    Update ArtistName.js

--- src/components/ArtistName/ArtistName.js
+++ src/components/ArtistName/ArtistName.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useCallback } from 'react'
 import ActionAreaCard from '../ActionAreaCard/ActionAreaCard'
 import Search from '../Search/Search'
 import './ArtistName.css'
